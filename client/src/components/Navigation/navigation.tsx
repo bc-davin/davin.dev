1c305c3c3ccd682535ef2afa3cb3f02cad230c3f
Davin <71451687+bc-davin@users.noreply.github.com>
2024-11-17 14:10:28 -0700

    #save the hamburger

--- client/src/components/Navigation/navigation.tsx
+++ client/src/components/Navigation/navigation.tsx
@@ -18,22 +18,22 @@ const Navigation: React.FC = () => {
             <div className="title">
                 <h1>Davin Meas</h1>
             </div>
-            <button className="hamburger" onClick={toggleMenu} aria-label="Toggle navigation menu">
-                &#9776;
-            </button>
-            <nav className={`navigation ${menuOpen ? 'open' : ''}`} aria-label="Main Navigation">
-                <ul>
-                    <li><NavLink end to="/" className={({ isActive }) => isActive ? "active" : ""}>Home</NavLink></li>
-                    <li><NavLink to="/about" className={({ isActive }) => isActive ? "active" : ""}>About Me</NavLink></li>
-                    <li><NavLink to="/projects" className={({ isActive }) => isActive ? "active" : ""}>Projects</NavLink></li>
-                    <li><NavLink to="/contact" className={({ isActive }) => isActive ? "active" : ""}>Contact</NavLink></li>
-                </ul>
-            </nav>
-            <div className="containerButton">
-                <button className="resumeButton" onClick={openResume} aria-label="View Resume">
-                    Resume
+                <button className="hamburger" onClick={toggleMenu} aria-label="Toggle navigation menu">
+                    &#9776;
                 </button>
-            </div>
+                <nav className={`navigation ${menuOpen ? 'open' : ''}`} aria-label="Main Navigation">
+                    <ul>
+                        <li><NavLink end to="/" className={({ isActive }) => isActive ? "active" : ""}>Home</NavLink></li>
+                        <li><NavLink to="/about" className={({ isActive }) => isActive ? "active" : ""}>About Me</NavLink></li>
+                        <li><NavLink to="/projects" className={({ isActive }) => isActive ? "active" : ""}>Projects</NavLink></li>
+                        <li><NavLink to="/contact" className={({ isActive }) => isActive ? "active" : ""}>Contact</NavLink></li>
+                    </ul>
+                    <div className="containerButton">
+                    <button className="resumeButton" onClick={openResume} aria-label="View Resume">
+                        Resume
+                    </button>
+                </div>
+                </nav>
         </div>
     );
 };
